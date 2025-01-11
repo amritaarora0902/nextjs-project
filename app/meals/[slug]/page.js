f936d6dbd823b75c45ecf3376140d356d104505d
amritaarora0902 <amritaarora2424@gmail.com>
2025-01-11 09:42:03 +0530

    add image picker

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealsDetailsPage({ params }) {
   const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
       <header className={classes.header}>
