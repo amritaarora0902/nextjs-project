90230d13c9343d69a93cb7253ee5fff688eaa1d3
amritaarora0902 <amritaarora2424@gmail.com>
2025-01-11 09:56:11 +0530

    handle form submission with server actions

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -13,7 +13,10 @@ export default function ImagePicker({ label, name }) {
 
   function handleImageChange(event) {
     const file = event.target.files[0];
-    if (!file) return;
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
 
     const fileReader = new FileReader();
 
@@ -41,6 +44,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
